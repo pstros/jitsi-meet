279055a807ef40010593519045ba2e084c5f58cc
Casey Watson <watsoncj@gmail.com>
2015-07-22 16:15:20 -0600

    extract auth domain to settings

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function init() {
 
     APP.desktopsharing.init();
     APP.RTC.start();
-    APP.xmpp.start();
+    APP.xmpp.start(APP.settings.getJid(), null);
     APP.statistics.start();
     APP.connectionquality.init();
     APP.keyboardshortcut.init();
